e8da49b97daf4e132b64b8c64177792b370e30e4
abhinavkgrd <abhinavk.grd@gmail.com>
2022-10-03 15:38:29 +0530

    add starter file

---
+++ starter.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define REP(i, a, b) for (int i = a; i < b; i++)
+
+ll MOD = 10e7 + 9;
+
+void setupFileIO()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+void setup()
+{
+    setupFileIO();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void solve()
+{
+    // solution goes here
+}
+
+int main()
+{
+    setup();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
