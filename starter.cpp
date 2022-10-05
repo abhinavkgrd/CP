fdd58f5477ec4c62c60eccff8ade0c7b28903a44
abhinavkgrd <abhinavk.grd@gmail.com>
2022-10-05 20:55:32 +0530

    remove some macros

--- starter.cpp
+++ starter.cpp
@@ -4,12 +4,19 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pi;
 
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
+#define MOD 10e7 + 9
 
-#define REP(i, a, b) for (int i = a; i < b; i++)
+void setupFileIO()
+{
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+}
+void setup()
+{
+    setupFileIO();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
 
 #define MOD 10e7 + 9
 
